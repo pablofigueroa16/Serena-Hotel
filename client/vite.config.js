233b57446eca3f50f60cdaa78588ff539bc21125
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-11-01 03:32:26 -0300

    fix: produccion

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes("node_modules")) {
+          return id.split("node_modules/")[1].split("/")[0].toString(); // Crea un chunk por cada módulo en node_modules
+        }
+      },
+    },
+  },
 });
